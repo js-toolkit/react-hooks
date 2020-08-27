e5b3009af8f31bca32e30968facda9011abc22f4
Vladimir <vzh.inbox@gmail.com>
2020-08-27 17:07:57 +0600

    Add useDoubleTap

---
+++ src/useDoubleTap.ts
@@ -0,0 +1,23 @@
+import { useCallback, useRef } from 'react';
+
+export default function useDoubleTap<T>(
+  handler: React.TouchEventHandler<T>,
+  delay = 300
+): React.TouchEventHandler<T> {
+  const lastTapTimeRef = useRef(0);
+
+  return useCallback<React.TouchEventHandler<T>>(
+    (event) => {
+      const currentTapTime = Date.now();
+      const tapLength = currentTapTime - lastTapTimeRef.current;
+
+      if (tapLength > 0 && tapLength <= delay) {
+        lastTapTimeRef.current = 0;
+        handler(event);
+      } else {
+        lastTapTimeRef.current = currentTapTime;
+      }
+    },
+    [delay, handler]
+  );
+}
