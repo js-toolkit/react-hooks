da75aa924ae381c9a768a2227d31207df7dcd445
Vladimir <vzh.inbox@gmail.com>
2020-09-02 13:20:47 +0600

    Fix toggleNativeSubtitles

--- src/useFullscreen.ts
+++ src/useFullscreen.ts
@@ -20,17 +20,17 @@ export interface Options extends FullscreenOptions {
 }
 
 function toggleNativeSubtitles(enabled: boolean, textTracks: TextTrackList): void {
-  console.log('toggleNativeSubtitles');
+  // console.log('toggleNativeSubtitles');
   for (let i = 0; i < textTracks.length; i += 1) {
     const track = textTracks[i];
-    console.log(track.label, track.mode);
     if (enabled && track.mode === 'hidden') {
       track.mode = 'showing';
     } else if (!enabled && track.mode === 'showing') {
       track.mode = 'hidden';
     }
+    // console.log(track.label, track.mode);
   }
-  console.log('toggleNativeSubtitles end');
+  // console.log('toggleNativeSubtitles end');
 }
 
 export default function useFullscreen(
