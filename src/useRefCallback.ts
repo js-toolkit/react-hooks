f0c4d9af1178a9c81c8ba8cab145e12fb3455753
Vladimir <vzh.inbox@gmail.com>
2021-03-04 15:18:03 +0600

    Add useRefCallback

---
+++ src/useRefCallback.ts
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+
+interface RefCallback<T extends AnyFunction> {
+  callee: T;
+  caller: T;
+}
+
+export default function useRefCallback<T extends AnyFunction, C = ThisParameterType<T>>(
+  callback: T,
+  context: C | undefined = undefined
+): T {
+  const ref = useRef<RefCallback<T>>();
+
+  if (!ref.current) {
+    ref.current = {
+      callee: callback,
+      // eslint-disable-next-line object-shorthand, func-names
+      caller: function (...args) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return ref.current!.callee.apply(context, args) as unknown;
+      } as T,
+    };
+  } else {
+    ref.current.callee = callback;
+  }
+
+  return ref.current.caller;
+}
