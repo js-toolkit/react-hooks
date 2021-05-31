1bb1eb973c411631db82908cac27f92049772b75
Vladimir <vzh.inbox@gmail.com>
2021-05-31 12:46:02 +0600

    Add update

--- src/useRefUpdate.ts
+++ src/useRefUpdate.ts
@@ -1,9 +1,25 @@
-import { useRef } from 'react';
+import { MutableRefObject, RefObject, useRef } from 'react';
 
-const useRefUpdate = function useUpdateRef(value: unknown): unknown {
+type UpdateRef<A, B> = (value: A) => B;
+
+function useRefUpdate<T>(initialValue: T, update?: UpdateRef<T, T>): MutableRefObject<T>;
+
+function useRefUpdate<T>(
+  initialValue: T | null,
+  update?: UpdateRef<T | null, T | null>
+): RefObject<T>;
+
+function useRefUpdate<T = undefined>(
+  update?: UpdateRef<T | undefined, T | undefined>
+): MutableRefObject<T | undefined>;
+
+function useRefUpdate<T>(
+  value: T | undefined | null,
+  update?: UpdateRef<T | undefined | null, T | undefined | null>
+): unknown {
   const ref = useRef(value);
-  ref.current = value;
+  ref.current = update ? update(value) : value;
   return ref;
-} as typeof useRef;
+}
 
 export default useRefUpdate;
