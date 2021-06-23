c7c7d5d49f8a82e816c6d53e84d4359594a39ef7
Vladimir <vzh.inbox@gmail.com>
2021-06-23 19:20:20 +0600

    Add show and refactor

--- src/useHideableState.tsx
+++ src/useHideableState.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useLayoutEffect, useMemo } from 'react';
 import useIsFirstMount from './useIsFirstMount';
+import useRefCallback from './useRefCallback';
 import useUpdateState from './useUpdateState';
 
 export interface HideableState {
@@ -7,45 +8,39 @@ export interface HideableState {
   readonly visible: boolean;
 }
 
-export type UseHideableStateResult = HideableState & { hide: VoidFunction; disable: VoidFunction };
+export type UseHideableStateResult = HideableState & {
+  show: VoidFunction;
+  hide: VoidFunction;
+  disable: VoidFunction;
+};
 
-function useHideableState(
+export default function useHideableState(
   state: HideableState | ((prevState?: HideableState) => HideableState),
-  updateStateDeps?: React.DependencyList
-): UseHideableStateResult;
-
-function useHideableState(
-  initialState: HideableState | (() => HideableState),
-  updateState: (prevState: HideableState) => HideableState,
-  updateStateDeps: React.DependencyList
-): UseHideableStateResult;
-
-function useHideableState(
-  initialState: HideableState | (() => HideableState),
-  updateStateOrDeps?: ((prevState: HideableState) => HideableState) | React.DependencyList,
-  updateStateDeps?: React.DependencyList
+  updateStateDeps: React.DependencyList = []
 ): UseHideableStateResult {
-  const [getState, setState] = useUpdateState(initialState);
+  const [getState, setState] = useUpdateState(state);
   const isFirstMount = useIsFirstMount();
 
-  const deps = Array.isArray(updateStateOrDeps) ? updateStateOrDeps : updateStateDeps ?? [];
-  const updateState = typeof updateStateOrDeps === 'function' ? updateStateOrDeps : initialState;
-
   useLayoutEffect(() => {
-    !isFirstMount() && setState(updateState);
+    !isFirstMount() && setState(state);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
+  }, updateStateDeps);
+
+  const show = useCallback(() => {
+    if (getState().visible) return;
+    setState((prev) => ({ ...prev, visible: true }));
+  }, [getState, setState]);
 
   const hide = useCallback(() => {
     if (!getState().visible) return;
     setState((prev) => ({ ...prev, visible: false }));
   }, [getState, setState]);
 
-  const disable = useCallback(() => {
+  const disable = useRefCallback(() => {
     const { enabled, visible } = getState();
     if (!enabled && !visible) return;
     setState((prev) => ({ ...prev, enabled: false, visible: false }));
-  }, [getState, setState]);
+  });
 
   return useMemo(
     () => ({
@@ -55,11 +50,10 @@ function useHideableState(
       get visible() {
         return getState().visible;
       },
+      show,
       hide,
       disable,
     }),
-    [disable, getState, hide]
+    [disable, getState, hide, show]
   );
 }
-
-export default useHideableState;
