c933d591ee4470c19428847153223665d6e2ad67
Vladimir <vzh.inbox@gmail.com>
2021-06-24 12:51:46 +0600

    Add slient option

--- src/useRefState.ts
+++ src/useRefState.ts
@@ -1,18 +1,28 @@
 import React, { useCallback, useRef } from 'react';
 import useUpdate from './useUpdate';
 
+interface SetRefStateOptions {
+  /** Do not re-render after state set. */
+  slient?: boolean;
+}
+
 type UpdateState<S> = (
   patch: IfExtends<
     S,
     ReadonlyArray<unknown> | AnyFunction,
     S,
     IfExtends<S, AnyObject, Partial<S>, S>
-  >
+  >,
+  options?: SetRefStateOptions
 ) => void;
 
 export default function useRefState<S>(
   initialState: S | (() => S)
-): [getState: () => S, setState: React.Dispatch<React.SetStateAction<S>>, patch: UpdateState<S>] {
+): [
+  getState: () => S,
+  setState: (state: React.SetStateAction<S>, options?: SetRefStateOptions) => void,
+  patch: UpdateState<S>
+] {
   const update = useUpdate();
 
   const stateRef = useRef<S>(
@@ -25,24 +35,24 @@ export default function useRefState<S>(
   const getState = useCallback(() => stateRef.current, []);
 
   const setState = useCallback(
-    (state: React.SetStateAction<S>) => {
+    (state: React.SetStateAction<S>, { slient }: SetRefStateOptions = {}) => {
       stateRef.current =
         typeof state === 'function'
           ? (state as React.ReducerWithoutAction<S>)(stateRef.current)
           : state;
-      update();
+      !slient && update();
     },
     [update]
   );
 
   const patchState = useCallback(
-    (patch: Partial<S>) => {
+    (patch: Partial<S>, { slient }: SetRefStateOptions = {}) => {
       if (patch != null && typeof patch === 'object') {
         Object.assign(stateRef.current, patch);
       } else {
         stateRef.current = patch;
       }
-      update();
+      !slient && update();
     },
     [update]
   );
