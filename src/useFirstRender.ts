0cf3ff7a0dcf824be7d60c91c91bbd319cf78880
Vladimir <vzh.inbox@gmail.com>
2021-06-25 12:44:55 +0600

    Refactor

--- src/useFirstRender.ts
+++ src/useFirstRender.ts
@@ -1,12 +1,1 @@
-import { useRef } from 'react';
-
-export default function useFirstRender(): boolean {
-  const isFirstRef = useRef(true);
-
-  if (isFirstRef.current) {
-    isFirstRef.current = false;
-    return true;
-  }
-
-  return isFirstRef.current;
-}
+export { default } from './useFirstMount';
