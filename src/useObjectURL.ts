60c3b12008a5bf97db7efcdd313e54168df6a488
Vladimir <vzh.inbox@gmail.com>
2021-06-25 14:41:02 +0600

    Add deps

--- src/useObjectURL.ts
+++ src/useObjectURL.ts
@@ -1,8 +1,11 @@
-import { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 
-export default function useObjectURL(getObject: () => Blob): string {
+export default function useObjectURL(
+  getObject: () => Blob,
+  deps: React.DependencyList = []
+): string {
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const url = useMemo(() => URL.createObjectURL(getObject()), []);
+  const url = useMemo(() => URL.createObjectURL(getObject()), deps);
   useEffect(() => () => URL.revokeObjectURL(url), [url]);
   return url;
 }
