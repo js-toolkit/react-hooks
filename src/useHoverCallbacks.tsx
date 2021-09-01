db4f40124bafcf8d93478a50ebaac9909cf6d36d
Vladimir <vzh.inbox@gmail.com>
2021-09-01 18:30:16 +0600

    Add useHoverCallbacks

---
+++ src/useHoverCallbacks.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export type UseHoverCallbacksProps<T> = [
+  onenter?: React.MouseEventHandler<T>,
+  onleave?: React.MouseEventHandler<T>
+];
+
+export type UseHoverCallbacksResult<T> = Pick<
+  React.HTMLAttributes<T>,
+  'onMouseEnter' | 'onMouseLeave'
+>;
+
+export default function useHoverCallbacks<T = Element>(
+  ...[onEnter, onLeave]: UseHoverCallbacksProps<T>
+): UseHoverCallbacksResult<T> {
+  return { onMouseEnter: onEnter, onMouseLeave: onLeave };
+}
