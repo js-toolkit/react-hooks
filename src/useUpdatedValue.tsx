b8fb94a32230faf77a84c081fc27996dcb7f390c
Vladimir <vzh.inbox@gmail.com>
2021-09-03 12:07:54 +0600

    Add useUpdatedRefValue and refactor

--- src/useUpdatedValue.tsx
+++ src/useUpdatedValue.tsx
@@ -1,17 +1,9 @@
-import React, { useMemo, useRef } from 'react';
+import React from 'react';
+import useUpdatedRefValue from './useUpdatedRefValue';
 
 export default function useUpdatedValue<S>(
   value: S | ((prevValue?: S) => S),
   deps: React.DependencyList = []
 ): S {
-  const valueRef = useRef<S>(undefined as unknown as S);
-
-  // Update Ref during render
-  useMemo(() => {
-    valueRef.current =
-      typeof value === 'function' ? (value as (prev?: S) => S)(valueRef.current) : value;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
-
-  return valueRef.current;
+  return useUpdatedRefValue(value, deps).current;
 }
