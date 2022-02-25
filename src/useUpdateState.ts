cdf985b496faa5867ef8acc30da72a0debd0d16e
Vladimir <vzh.inbox@gmail.com>
2022-02-25 09:26:16 +0600

    Refactor useUpdateState

--- src/useUpdateState.ts
+++ src/useUpdateState.ts
@@ -1,6 +1,6 @@
-import { useState, useCallback, SetStateAction, Dispatch } from 'react';
+import type { SetStateAction, Dispatch } from 'react';
 import useRefCallback from './useRefCallback';
-import useUpdatedRef from './useUpdatedRef';
+import useRefState from './useRefState';
 
 export type StateGetter<E, S> = (wrappedOrValue: E) => S;
 
@@ -8,7 +8,7 @@ export type UpdateState<E> = (wrappedOrValue: E) => void;
 
 export function getValue<S>(wrappedOrValue: { value: S } | S): S {
   if (typeof wrappedOrValue === 'object' && 'value' in wrappedOrValue) return wrappedOrValue.value;
-  // Do not throw error because `S` may be an `object` type or any simple type.
+  // Do not throw an error because `S` may be an `object` type or any simple type.
   return wrappedOrValue;
 }
 
@@ -16,12 +16,9 @@ export default function useUpdateState<S, E = { value: S } | S>(
   initialState: S | (() => S),
   stateGetter: StateGetter<E, S> = getValue as StateGetter<E, S>
 ): [getState: () => S, setState: Dispatch<SetStateAction<S>>, updateState: UpdateState<E>] {
-  const [state, setState] = useState(initialState);
-  const stateRef = useUpdatedRef(state);
+  const [getState, setState] = useRefState(initialState);
 
   const updateState = useRefCallback<UpdateState<E>>((event) => setState(stateGetter(event)));
-
-  const getState = useCallback(() => stateRef.current, [stateRef]);
 
   return [getState, setState, updateState];
 }
