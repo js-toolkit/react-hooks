28fafff1f9f442ec65efa4a88f29ba9b2246ee90
Vladimir <vzh.inbox@gmail.com>
2022-07-27 15:54:35 +0600

    Add useRafCallback

---
+++ src/useRafCallback.ts
@@ -0,0 +1,20 @@
+import { DependencyList, useCallback, useEffect, useRef } from 'react';
+
+export default function useRafCallback<T extends AnyFunction>(
+  callback: T,
+  deps: DependencyList
+): T {
+  const frameRef = useRef(0);
+
+  useEffect(() => () => cancelAnimationFrame(frameRef.current), []);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useCallback(
+    ((...args: unknown[]) => {
+      frameRef.current = requestAnimationFrame(() => {
+        callback(...args);
+      });
+    }) as T,
+    deps
+  );
+}
