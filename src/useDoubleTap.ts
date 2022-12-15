9a8061c2d12953f5c80bec6c3f20f5b2bf454f0a
Vladimir <vzh.inbox@gmail.com>
2022-12-15 16:14:42 +0600

    Improve useDoubleTap

--- src/useDoubleTap.ts
+++ src/useDoubleTap.ts
@@ -1,22 +1,31 @@
-import { useCallback, useRef } from 'react';
+import { useCallback, useMemo, useRef } from 'react';
 
 export default function useDoubleTap<T>(
-  handler: React.TouchEventHandler<T>,
-  delay = 300,
+  factory: () => {
+    handler: React.TouchEventHandler<T>;
+    /** Default `300` */
+    delay?: number;
+  },
   deps: React.DependencyList = []
 ): React.TouchEventHandler<T> {
   const lastTapTimeRef = useRef(0);
 
-  return useCallback<React.TouchEventHandler<T>>((event) => {
-    const currentTapTime = Date.now();
-    const tapLength = currentTapTime - lastTapTimeRef.current;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const { handler, delay = 300 } = useMemo(factory, deps);
 
-    if (tapLength > 0 && tapLength <= delay) {
-      lastTapTimeRef.current = 0;
-      handler(event);
-    } else {
-      lastTapTimeRef.current = currentTapTime;
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
+  return useCallback<React.TouchEventHandler<T>>(
+    (event) => {
+      const currentTapTime = Date.now();
+      const tapLength = currentTapTime - lastTapTimeRef.current;
+
+      if (tapLength > 0 && tapLength <= delay) {
+        // lastTapTimeRef.current = 0;
+        lastTapTimeRef.current = currentTapTime;
+        handler(event);
+      } else {
+        lastTapTimeRef.current = currentTapTime;
+      }
+    },
+    [delay, handler]
+  );
 }
