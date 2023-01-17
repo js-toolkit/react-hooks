29e13a83bc00db219f7fcc03198286cde7659fea
Vladimir <vzh.inbox@gmail.com>
2023-01-17 11:14:42 +0600

    Improve useScreenSize

--- src/useScreenSize.ts
+++ src/useScreenSize.ts
@@ -1,25 +1,68 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { rafCallback } from '@jstoolkit/web-utils/rafCallback';
+import useRefState from './useRefState';
 
 export type ScreenSize = Pick<Screen, 'width' | 'height'>;
 
-export default function useScreenSize(): ScreenSize {
-  const [size, setSize] = useState<ScreenSize>({
+export interface UseScreenSizeProps {
+  readonly respectOrientation?: boolean;
+}
+
+function angleToOrientationType(angle: number): OrientationType | undefined {
+  if (angle === 0) return 'portrait-primary';
+  if (angle === 180) return 'portrait-secondary';
+  if (angle === 90) return 'landscape-primary';
+  if (angle === -90) return 'landscape-secondary';
+  return undefined;
+}
+
+export default function useScreenSize({
+  respectOrientation = true,
+}: UseScreenSizeProps = {}): ScreenSize {
+  const [getSize, setSize] = useRefState<ScreenSize>(() => ({
     width: window.screen.width,
     height: window.screen.height,
-  });
+  }));
 
   useEffect(() => {
-    const changeHandler = (): void => {
-      const { width, height } = window.screen;
+    const updateSize = (width: number, height: number): void => {
+      const prevSize = getSize();
+      if (prevSize.width === width && prevSize.height === height) return;
       setSize({ width, height });
     };
 
-    window.addEventListener('orientationchange', changeHandler);
+    const changeHandler = rafCallback(() => {
+      const { width, height, orientation } = window.screen;
+
+      if (!respectOrientation) {
+        updateSize(width, height);
+        return;
+      }
+
+      const orientationType = orientation ? orientation.type : angleToOrientationType(orientation);
+      if (
+        (orientationType === 'landscape-primary' || orientationType === 'landscape-secondary') &&
+        width < height
+      ) {
+        updateSize(height, width);
+      }
+    });
+
+    if (window.screen.orientation) {
+      window.screen.orientation.addEventListener('change', changeHandler);
+    } else {
+      window.addEventListener('orientationchange', changeHandler);
+    }
 
     return () => {
-      window.removeEventListener('orientationchange', changeHandler);
+      changeHandler.cancel();
+      if (window.screen.orientation) {
+        window.screen.orientation.removeEventListener('change', changeHandler);
+      } else {
+        window.removeEventListener('orientationchange', changeHandler);
+      }
     };
-  }, []);
+  }, [getSize, respectOrientation, setSize]);
 
-  return size;
+  return getSize();
 }
