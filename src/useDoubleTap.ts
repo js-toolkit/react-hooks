24d92ba3b415e19efcd693cf5aeab746bd3e13b5
Vladimir <vzh.inbox@gmail.com>
2023-01-31 16:51:42 +0600

    Improve useDoubleTap

--- src/useDoubleTap.ts
+++ src/useDoubleTap.ts
@@ -1,34 +1,46 @@
-import { useCallback, useMemo, useRef } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 
-export default function useDoubleTap<T>(
+type BaseEvent = Pick<React.TouchEvent, 'timeStamp'>;
+type BaseHandler<E> = (event: E) => any;
+
+export default function useDoubleTap<
+  E extends BaseEvent,
+  H extends BaseHandler<E> = BaseHandler<E>
+>(
   factory: () => {
-    handler: React.TouchEventHandler<T>;
-    tapHandler?: (...params: Parameters<React.TouchEventHandler<T>>) => boolean | void;
+    handler: H;
+    tapHandler?: (...params: Parameters<H>) => boolean | void;
     /** Default `300` */
     delay?: number;
   },
   deps: React.DependencyList = []
-): React.TouchEventHandler<T> {
+): H {
   const lastTimeRef = useRef(0);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const { handler, tapHandler, delay = 300 } = useMemo(factory, deps);
 
-  return useCallback<React.TouchEventHandler<T>>(
-    (event) => {
-      const res = tapHandler && tapHandler(event);
-      if (res === false) return;
-
+  return useCallback(
+    (...params: Parameters<H>) => {
+      const event = params[0];
       const currentTime = event.timeStamp;
       const delta = currentTime - lastTimeRef.current;
+      const doubleClick = delta > 0 && delta <= delay;
 
-      if (delta > 0 && delta <= delay) {
+      if (doubleClick) {
         lastTimeRef.current = 0;
         handler(event);
-      } else {
-        lastTimeRef.current = currentTime;
+        return;
       }
+
+      const res = tapHandler && tapHandler(...params);
+      if (res === false) {
+        lastTimeRef.current = 0;
+        return;
+      }
+
+      lastTimeRef.current = currentTime;
     },
     [delay, handler, tapHandler]
-  );
+  ) as H;
 }
