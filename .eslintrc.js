1beb1b56c052d00bceba33c7c2ac9d86b148032a
Vladimir <vzh.inbox@gmail.com>
2023-09-12 11:11:57 +0600

    Update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: require.resolve('@js-toolkit/configs/eslint/react'),
+  ignorePatterns: ['/dist', '.eslintrc.js'],
   rules: {
     'react-hooks/exhaustive-deps': [
       'error',
