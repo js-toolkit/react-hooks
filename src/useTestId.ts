98072b31fc8bf26869ce14a6b33155d152c6c22f
Vladimir <vzh.inbox@gmail.com>
2024-01-10 16:24:42 +0600

    Remove useTestId

--- src/useTestId.ts
+++
@@ -1,5 +0,0 @@
-import appEnv from '@js-toolkit/configs/appEnv';
-
-export function useTestId(id: string | undefined): { 'data-testid': typeof id } | undefined {
-  return appEnv.ifTest({ 'data-testid': id }, undefined);
-}
