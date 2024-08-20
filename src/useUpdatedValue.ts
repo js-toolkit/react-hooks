633cd16197bc520e91e72a5d4161d5f18433f894
Vladimir <vzh.inbox@gmail.com>
2024-08-21 00:38:56 +0500

    Remove useUpdatedValue

--- src/useUpdatedValue.ts
+++
@@ -1,8 +0,0 @@
-import useUpdatedRefValue from './useUpdatedRefValue';
-
-export default function useUpdatedValue<S>(
-  value: S | ((prevValue?: S) => S),
-  deps: React.DependencyList = []
-): S {
-  return useUpdatedRefValue(value, deps).current;
-}
