aef0de96186d522ddb397c79b20be28676027714
Vladimir <vzh.inbox@gmail.com>
2024-11-05 11:44:32 +0500

    Improve useLongPress

--- src/useLongPress.ts
+++ src/useLongPress.ts
@@ -1,6 +1,7 @@
 import React from 'react';
-import { debounce } from '@js-toolkit/utils/debounce';
+import { noop } from '@js-toolkit/utils/noop';
 import { hasIn } from '@js-toolkit/utils/hasIn';
+import { debounce } from '@js-toolkit/utils/debounce';
 // import { preventDefault } from '@js-toolkit/web-utils/preventDefault';
 import useMemoDestructor from './useMemoDestructor';
 import useRefCallback from './useRefCallback';
@@ -28,6 +29,10 @@ export interface UseLongPressResult<E /* extends BaseEvent */> {
   readonly isPressed: () => boolean;
 }
 
+interface LongPressResult<E> extends UseLongPressResult<E> {
+  readonly cancel: VoidFunction;
+}
+
 // function isEventLike(event: unknown): event is Pick<Event, 'preventDefault'> {
 //   return hasIn(event as Event, 'preventDefault');
 // }
@@ -39,6 +44,84 @@ function isPointerEventLike(
     hasIn(event as React.PointerEvent, 'pointerId') &&
     hasIn(event as React.PointerEvent, 'pointerType')
   );
+}
+
+export function getLongPress<E, D = unknown>(
+  callback: (event: E) => D,
+  {
+    delay = 500,
+    // preventDefault: preventDefaultProp,
+    captureEvents = true,
+    filter,
+    onCancel,
+    onEnd,
+    fallback,
+  }: UseLongPressProps<E, D> = {}
+): LongPressResult<E> {
+  let pressed = false;
+  // let prevented = false;
+  // It needs for if event is filtered out.
+  let callbackInvoked = false;
+  let callbackData: D;
+
+  const debouncedCallback = debounce((event: E, currentTarget: Element | undefined): D => {
+    if (captureEvents && currentTarget && isPointerEventLike(event)) {
+      currentTarget.setPointerCapture(event.pointerId);
+    }
+    callbackInvoked = true;
+    callbackData = callback(event);
+    return callbackData;
+  }, delay);
+
+  return {
+    onDown: (event) => {
+      pressed = true;
+      if (filter && !filter(event)) return;
+
+      // if (preventDefaultProp && isEventLike(event)) {
+      //   preventDefault(event);
+      //   prevented = true;
+      // }
+
+      callbackInvoked = false;
+      (event as React.SyntheticEvent)?.persist();
+      debouncedCallback(event, (event as React.SyntheticEvent).currentTarget);
+    },
+    onUp: (event) => {
+      if (isPointerEventLike(event) && event.currentTarget.hasPointerCapture(event.pointerId)) {
+        event.currentTarget.releasePointerCapture(event.pointerId);
+      }
+
+      pressed = false;
+
+      // if (prevented && isEventLike(event)) {
+      //   preventDefault(event);
+      //   prevented = false;
+      // }
+
+      if (debouncedCallback.isPending) {
+        debouncedCallback.cancel();
+        if (onCancel) onCancel(event);
+      } else if (callbackInvoked) {
+        if (onEnd) onEnd(event, callbackData);
+      } else if (fallback) {
+        fallback(event);
+      }
+
+      callbackInvoked = false;
+    },
+    isPressed: () => pressed,
+    cancel: () => {
+      debouncedCallback.cancel();
+      // pressed = false;
+      // callbackInvoked = false;
+      // callbackData = undefined as D;
+    },
+  };
+}
+
+function trueFn(): boolean {
+  return true;
 }
 
 export default function useLongPress<E /* extends BaseEvent */, D = unknown>(
@@ -54,74 +137,27 @@ export default function useLongPress<E /* extends BaseEvent */, D = unknown>(
   }: UseLongPressProps<E, D> = {}
 ): UseLongPressResult<E> {
   const callbackRef = useRefCallback(callback);
-
-  const filterRef = React.useRef(filter);
-  filterRef.current = filter;
-
-  const onCancelRef = React.useRef(onCancel);
-  onCancelRef.current = onCancel;
-
-  const onEndRef = React.useRef(onEnd);
-  onEndRef.current = onEnd;
-
-  const fallbackRef = React.useRef(fallback);
-  fallbackRef.current = fallback;
+  const filterRef = useRefCallback(filter ?? trueFn);
+  const onCancelRef = useRefCallback(onCancel ?? noop);
+  const onEndRef = useRefCallback(onEnd ?? noop);
+  const fallbackRef = useRefCallback(fallback ?? noop);
 
   return useMemoDestructor(() => {
-    let pressed = false;
-    // let prevented = false;
-    // It needs for if event is filtered out.
-    let callbackInvoked = false;
-    let callbackData: D;
-    const debouncedCallback = debounce((event: E, currentTarget: Element | undefined): D => {
-      if (captureEvents && currentTarget && isPointerEventLike(event)) {
-        currentTarget.setPointerCapture(event.pointerId);
-      }
-      callbackInvoked = true;
-      callbackData = callbackRef(event);
-      return callbackData;
-    }, delay);
+    const longPress = getLongPress(callbackRef, {
+      delay,
+      captureEvents,
+      filter: filterRef,
+      onCancel: onCancelRef,
+      onEnd: onEndRef,
+      fallback: fallbackRef,
+    });
     return [
       {
-        onDown: (event) => {
-          pressed = true;
-          if (filterRef.current && !filterRef.current(event)) return;
-
-          // if (preventDefaultProp && isEventLike(event)) {
-          //   preventDefault(event);
-          //   prevented = true;
-          // }
-
-          callbackInvoked = false;
-          (event as React.SyntheticEvent)?.persist();
-          debouncedCallback(event, (event as React.SyntheticEvent).currentTarget);
-        },
-        onUp: (event) => {
-          if (isPointerEventLike(event) && event.currentTarget.hasPointerCapture(event.pointerId)) {
-            event.currentTarget.releasePointerCapture(event.pointerId);
-          }
-
-          pressed = false;
-
-          // if (prevented && isEventLike(event)) {
-          //   preventDefault(event);
-          //   prevented = false;
-          // }
-
-          if (debouncedCallback.isPending) {
-            debouncedCallback.cancel();
-            if (onCancelRef.current) onCancelRef.current(event);
-          } else if (callbackInvoked) {
-            if (onEndRef.current) onEndRef.current(event, callbackData);
-          } else if (fallbackRef.current) {
-            fallbackRef.current(event);
-          }
-
-          callbackInvoked = false;
-        },
-        isPressed: () => pressed,
+        onDown: longPress.onDown,
+        onUp: longPress.onUp,
+        isPressed: longPress.isPressed,
       },
-      () => debouncedCallback.cancel(),
+      longPress.cancel,
     ];
-  }, [callbackRef, delay]);
+  }, [callbackRef, captureEvents, delay, fallbackRef, filterRef, onCancelRef, onEndRef]);
 }
