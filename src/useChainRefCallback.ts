773cedc53e1e15e85279b75749dcba41733faca2
Vladimir <vzh.inbox@gmail.com>
2025-01-22 09:45:50 +0500

    Update useChainRefCallback

--- src/useChainRefCallback.ts
+++ src/useChainRefCallback.ts
@@ -1,10 +1,11 @@
 import useRefCallback from './useRefCallback';
 
-export default function useChainRefCallback<T extends (...args: unknown[]) => void>(
+export default function useChainRefCallback<T extends AnyFunction>(
   ...callbacks: (T | undefined)[]
 ): T {
   return useRefCallback(((...args) => {
     callbacks.forEach((cb) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
       cb && cb(...args);
     });
   }) as T);
