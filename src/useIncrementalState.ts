102c2518d4dbf8c0fe5b8e05ee8bc9c8aba9fcb5
Vladimir <vzh.inbox@gmail.com>
2025-01-27 17:56:36 +0500

    Improve useIncrementalState

--- src/useIncrementalState.ts
+++ src/useIncrementalState.ts
@@ -1,18 +1,19 @@
 import useExtendedState from './useExtendedState';
-import useRefState from './useRefState';
+import useRefState, { type SetRefStateOptions } from './useRefState';
 
 export interface UseIncrementalStateResult {
-  inc: VoidFunction;
+  inc: (options?: SetRefStateOptions) => void;
   readonly value: number;
 }
 
 export default function useIncrementalState(initialState = 0): UseIncrementalStateResult {
   const [store] = useExtendedState(useRefState(initialState), ({ get, set }) => ({
-    inc: () => set((prev) => (prev >= Number.MAX_SAFE_INTEGER ? 1 : prev + 1)),
+    inc: (options?: SetRefStateOptions) => {
+      set((prev) => (prev >= Number.MAX_SAFE_INTEGER ? 1 : prev + 1), options);
+    },
     get value() {
       return get();
     },
   }));
-
   return store;
 }
