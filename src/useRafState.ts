ebee3be20ee2b58cffa556ae525aa56ec80a79cc
Vladimir <vzh.inbox@gmail.com>
2025-07-29 13:51:53 +0500

    Update result of useRafState

--- src/useRafState.ts
+++ src/useRafState.ts
@@ -1,30 +1,33 @@
 import useRafCallback from './useRafCallback';
 import useRefState, { type SetRefStateOptions } from './useRefState';
 
+type SetState<F extends AnyFunction> = ReturnType<typeof useRafCallback<F>>;
+
 export default function useRafState<S = undefined>(): [
   getState: () => S | undefined,
-  setState: (nextState: React.SetStateAction<S | undefined>, options?: SetRefStateOptions) => void,
+  setState: SetState<
+    (nextState: React.SetStateAction<S | undefined>, options?: SetRefStateOptions) => void
+  >,
 ];
 
 export default function useRafState<S>(
   initialState: S | (() => S)
 ): [
   getState: () => S,
-  setState: (nextState: React.SetStateAction<S>, options?: SetRefStateOptions) => void,
+  setState: SetState<(nextState: React.SetStateAction<S>, options?: SetRefStateOptions) => void>,
 ];
 
 export default function useRafState<S>(
   initialState?: S | (() => S)
 ): [
   getState: () => S | undefined,
-  setState: (nextState: React.SetStateAction<S | undefined>, options?: SetRefStateOptions) => void,
+  setState: SetState<
+    (nextState: React.SetStateAction<S | undefined>, options?: SetRefStateOptions) => void
+  >,
 ] {
   const [getState, setState] = useRefState(initialState);
 
-  const setRafState = useRafCallback<typeof setState>(
-    (value, options) => setState(value, options),
-    [setState]
-  );
+  const setRafState = useRafCallback<typeof setState>(setState, [setState]);
 
   return [getState, setRafState];
 }
